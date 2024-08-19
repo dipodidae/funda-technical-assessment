109785b2f0dd6abfeea27eaeacac8ae8dc13bda7
dpdd <dpdd@squat.net>
2024-08-19 10:00:06 +0200

    Update index page content and fix typo in link URL

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
 <template>
-  <div>
-    You can check out <ULink to="/houses/6289a7bb-a1a8-40d5-bed1-bff3a5f62ee6">
-      6289a7bb-a1a8-40d5-bed1-bff3a5f62ee6
-    </ULink> (but this house is gone).
+  <div class="px-4 lg:px-0 max-w-[100ch] mx-auto">
+    <h1 class="text-xl mb-4">
+      Welcome to my tech assessment
+    </h1>
+    <p>
+      You can check out <ULink to="/houses/6289a7bb-a1a8-40d5-bed1-bff3a5f62ee6">
+        6289a7bb-a1a8-40d5-bed1-bff3a5f62ee6
+      </ULink> (but this house is gone, and there is no proper error handling.
+    </p>
+    <p>You can also just click on 'houses' in the top meu</p>
   </div>
 </template>
